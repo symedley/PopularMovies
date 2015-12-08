8c4572fbbde5a892782184c3380d774d0bb12921
Susannah.Medley <symedley@gmail.com>
2015-12-07 19:39:00 -0800

    Add a class to hold the API Key. This way, I can put the placeholder string on GitHub and keep a copy of this class with my API key locally.

---
+++ app/src/main/java/com/example/susannah/popularmovies/ApiKey.java
@@ -0,0 +1,8 @@
+package com.example.susannah.popularmovies;
+
+/**
+ * Created by Susannah on 12/7/2015.
+ */
+public class ApiKey {
+    public static String API_KEY = "Add your API Key here";
+}
---
+++ app/src/main/java/com/example/susannah/popularmovies/FetchMovieTask.java
@@ -0,0 +1,7 @@
+package com.example.susannah.popularmovies;
+
+/**
+ * Created by Susannah on 12/3/2015.
+ */
+public class FetchMovieTask {
+}
