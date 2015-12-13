b7035d33b6124b379f1d2033a5bd599ba752b2d5
Susannah.Medley <symedley@gmail.com>
2015-12-13 15:27:06 -0800

    Parse the JSON string and , as a temp measure, put the movie names in a results string to be displayed with a dummy icon.

--- app/src/main/java/com/example/susannah/popularmovies/ApiKey.java
+++ app/src/main/java/com/example/susannah/popularmovies/ApiKey.java
@@ -4,5 +4,5 @@ package com.example.susannah.popularmovies;
  * Created by Susannah on 12/7/2015.
  */
 public class ApiKey {
-    public static String API_KEY = "Add your API Key here";
+    public static String API_KEY = "0bc11809ea45fa31a8aa9f2c1766a4e3";
 }
--- app/src/main/java/com/example/susannah/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/susannah/popularmovies/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
