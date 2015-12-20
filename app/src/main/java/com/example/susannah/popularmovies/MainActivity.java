8edd6f6af3581dfe747e3c9cd3bd2566cb0e7a18
Susannah.Medley <symedley@gmail.com>
2015-12-19 23:04:19 -0800

    When Settings is selected from the menu, it actually does something. Create SettingsActivity. A lot of these xml files were generated automatically and could be deleted.

--- app/src/main/java/com/example/susannah/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/susannah/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.susannah.popularmovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -36,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
             return true;
         }
 
