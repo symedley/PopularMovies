961e2d82c63d9681a53a3cde5ee9a6265787bf14
Susannah.Medley <symedley@gmail.com>
2016-01-06 23:00:15 -0800

    the wronmg version of ApiKey got onto github. trying to roll it back.

--- app/src/main/java/com/example/susannah/popularmovies/ApiKey.java
+++ app/src/main/java/com/example/susannah/popularmovies/ApiKey.java
@@ -1,11 +1,8 @@
 package com.example.susannah.popularmovies;
 
-/** ApiKey holds the key that was assigned to me when I registered with the movie database.
- * .gitignore tells git to not update this file. This makes it easier to not accidentally
- * put my API KEY into GitHub.
- *
+/**
  * Created by Susannah on 12/7/2015.
  */
 public class ApiKey {
-    public static String API_KEY = "***REMOVED***";
+    public static String API_KEY = "Add your API Key here";
 }
