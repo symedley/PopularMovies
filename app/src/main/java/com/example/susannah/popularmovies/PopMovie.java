3168005bbcc93630fcf2e7104f5ece2c8ad0ccfa
Susannah.Medley <symedley@gmail.com>
2016-01-13 22:33:32 -0800

    make PopMovie Parcelable in preparation for saving it for screen rotations.

--- app/src/main/java/com/example/susannah/popularmovies/PopMovie.java
+++ app/src/main/java/com/example/susannah/popularmovies/PopMovie.java
@@ -1,14 +1,15 @@
 package com.example.susannah.popularmovies;
 
 import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /** PopMovie holds the data for one retrieved movie. Created when parsing JSON from the movie database.
- * <p/>
+ *
  * Created by Susannah on 11/29/2015.
  */
-public class PopMovie {
+public class PopMovie implements Parcelable {
 
-    int thumb; // integer reference to a thumbnail image
     String posterPath;
     String posterPathUri;
 
@@ -25,6 +26,7 @@ public class PopMovie {
     int voteCount;
     boolean video;
     float voteAverage;
+    int thumb; // integer reference to a thumbnail image
 
     public PopMovie(
             String vPosterPath,
@@ -87,4 +89,92 @@ public class PopMovie {
         // TODO deal better with the dummy image
         thumb = R.drawable.thumb;
     }
+
+    /** private creator called when creating the object from the parcel
+     *
+     * @param parcelIn
+     * Read all the data from the parcel in the same order it was written.
+     */
+    private PopMovie( Parcel parcelIn){
+        posterPath = parcelIn.readString();
+        parcelIn.writeString(posterPathUri);
+        int adultInt = parcelIn.readInt();
+        adult = (adultInt==1) ? true : false;
+
+        overview = parcelIn.readString();
+        releaseDate = parcelIn.readString();
+
+        genreIds = parcelIn.createIntArray();
+
+        tmdId = parcelIn.readInt();
+        origTitle = parcelIn.readString();
+        origLang = parcelIn.readString();
+        title = parcelIn.readString();
+        backdropPath = parcelIn.readString();
+        // LEFT OFF. TODO
+        popularity = parcelIn.readFloat();
+        voteCount = parcelIn.readInt();
+
+        int videoInt = parcelIn.readInt();
+        video = (videoInt==1) ? true : false;
+
+        voteAverage = parcelIn.readFloat();
+        thumb = parcelIn.readInt();
+    }
+
+    @Override
+    /** describeContents gives you a bitmask (?) is not required for this project
+     *
+     */
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    /** writeToParcel
+     *
+     */
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(posterPath);
+        dest.writeString(posterPathUri);
+
+        int adultInt = (adult) ? 1 : 0;
+        dest.writeInt(adultInt);
+
+        dest.writeString(overview);
+        dest.writeString(releaseDate);
+        dest.writeIntArray(genreIds);
+        dest.writeInt(tmdId);
+        dest.writeString( origTitle);
+        dest.writeString( origLang);
+        dest.writeString(title);
+        dest.writeString(backdropPath);
+        // LEFT OFF. TODO
+        dest.writeFloat(popularity);
+        dest.writeInt( voteCount);
+
+        int videoInt = (video) ? 1 : 0;
+        dest.writeInt(  videoInt);
+
+        dest.writeFloat(voteAverage);
+        dest.writeInt( thumb); // integer reference to a thumbnail image
+    }
+
+    /**
+     * Parcelable.Creator is an interface that must be defined. It generates instances of PopMovie from a parcel.
+     * It creates an object called CREATOR object which is of type PopMovie.
+     * 2 required methods
+     * newArray will not be useful because we're using an ArrayAdapter which requires a ListArray (?)
+     */
+    public final Parcelable.Creator<PopMovie> CREATOR = new Creator<PopMovie>() {
+        @Override
+        public PopMovie createFromParcel(Parcel source) {
+            return new PopMovie(source);
+        }
+
+        @Override
+        public PopMovie[] newArray(int size) {
+            return new PopMovie[size];
+        }
+    };
 }
